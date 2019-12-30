83172093d2e56c3d18c304aafc46f16c29b0d41a
hongguan.liu <hongguan.liu@weimob.com>
2019-12-30 11:03:30 +0800

    修改代码错误

--- decoders/seg_detector.py
+++ decoders/seg_detector.py
@@ -10,7 +10,8 @@ BatchNorm2d = nn.BatchNorm2d
 class SeparableConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(SeparableConv2d, self).__init__()
-        self.depthwise_conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, groups=planes,
+        self.depthwise_conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1,
+                                        groups=in_channels,
                                         bias=False)
         self.pointwise_conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
         self.bn = nn.BatchNorm2d(out_channels)
