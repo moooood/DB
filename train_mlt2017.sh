3d43b87909fc3bb26f2fa20e6222d75280dfab6d
hongguan.liu <hongguan.liu@weimob.com>
2019-12-30 11:40:51 +0800

    train bash file modified

--- train_mlt2017.sh
+++ train_mlt2017.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 nohup python -u train.py experiments/seg_detector/mlt2017_resnet18_conv2d.yaml\
-    --num_gpus 1\
+CUDA_VISIBLE_DEVICES=0,1 nohup python -u train.py experiments/seg_detector/mlt2017_resnet18_conv2d.yaml\
+    --num_gpus 2\
     >train_mlt2017.log 2>&1 &
